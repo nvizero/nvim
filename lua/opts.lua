5301483c21f951d16bdf24dc56aa73adb173913d
victor20220919 <victor20220919@gmail.com>
2022-12-26 13:47:14 +0800

    neo test

--- lua/opts.lua
+++ lua/opts.lua
@@ -33,3 +33,13 @@ require("indent_blankline").setup {
     show_current_context = true,
     show_current_context_start = true,
 }
+require("neotest")
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+})
--- lua/plug.lua
+++ lua/plug.lua
@@ -133,4 +133,39 @@ return require('packer').startup(function()
 		    require('nvim-terminal').setup()
 	    end,
     }
+		use {
+			"nvim-neotest/neotest",
+			requires = {
+				"nvim-lua/plenary.nvim",
+				"nvim-treesitter/nvim-treesitter",
+				"antoinemadec/FixCursorHold.nvim"
+			}
+		}
+		use { "williamboman/mason.nvim" }
+		use({
+			"nvim-neotest/neotest",
+			requires = {
+				"nvim-neotest/neotest-go",
+				-- Your other test adapters here
+			},
+			config = function()
+				-- get neotest namespace (api call creates or returns namespace)
+				local neotest_ns = vim.api.nvim_create_namespace("neotest")
+				vim.diagnostic.config({
+					virtual_text = {
+						format = function(diagnostic)
+							local message =
+							diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+							return message
+						end,
+					},
+				}, neotest_ns)
+				require("neotest").setup({
+					-- your neotest config here
+					adapters = {
+						require("neotest-go"),
+					},
+				})
+			end,
+		})
 end)
