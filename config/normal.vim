836fb78411154eef0add88791d2e5ff954ddae7c
yen chi tseng <yenchitseng@yendebijixingdiannao.local>
2024-08-27 14:29:32 +0800

    fix

--- config/normal.vim
+++ config/normal.vim
@@ -4,3 +4,6 @@ set termguicolors
 hi TabLine guifg=#fadfc2 guibg=#7d8e7b gui=underline
 hi TabLineSel guifg=#ffffff guibg=#6c698a gui=bold,italic
 hi TabLineFill guifg=#2dbd1e guibg=#ffffff
+
+inoremap <silent><expr> <TAB> pumvisible() ? "\<C-n>" : "\<TAB>"
+inoremap <silent><expr> <S-TAB> pumvisible() ? "\<C-p>" : "\<S-TAB>"
--- init.lua
+++ init.lua
@@ -82,11 +82,14 @@ cmp.setup({
         end,
     },
     mapping = {
+        ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+
         ['<C-p>'] = cmp.mapping.select_prev_item(),
         ['<C-n>'] = cmp.mapping.select_next_item(),
         -- Add tab support
-        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-        ['<Tab>'] = cmp.mapping.select_next_item(),
+        -- ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        -- ['<Tab>'] = cmp.mapping.select_next_item(),
         ['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
